2f26c9fb3a27b71ce93268af8266e41a43d4096a
Joe <laulettaj@gmail.com>
2024-05-07 12:59:43 -0400

    fix footer position

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="footer">
+    <footer>
       Copyright &copy; {new Date().getFullYear()} Skedge
     </footer>
   )
